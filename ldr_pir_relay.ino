533f4bcd2ed93994df0852a16863a465eda8f6a6
Mahesh Chandra <118873892+ChandraMahesh24@users.noreply.github.com>
2023-02-21 13:42:11 +0530

    Update ldr_pir_relay.ino

--- ldr_pir_relay.ino
+++ ldr_pir_relay.ino
@@ -1,5 +1,7 @@
-int LED=  3 ; // choose the pin for the RELAY
-int PIR =5  ; // choose the input pin (for PIR sensor)                 
+int LED=  9 ; // choose the pin for the RELAY
+int PIR =3  ; // choose the input pin (for PIR sensor) 
+int pirState = LOW;   
+int value_pir=LOW;              
 void setup()
 {    
 Serial.begin(9600);
@@ -9,24 +11,26 @@ Serial.begin(9600);
 void loop() 
 {
     
-  int value_ldr = analogRead(A4); // read LDR value
-  int value_pir = digitalRead(PIR); // read input value
+  int value_ldr = analogRead(A0); // read LDR value
+  value_pir = digitalRead(PIR); // read input value
   Serial.println(value_ldr);
   Serial.println(value_pir);
-
- if(700<value_ldr){ 
-   value_pir==LOW;
-    if( value_pir==HIGH){
-       digitalWrite(LED,HIGH);  // Turn ON the light
-       delay(3000);        
+//  delay(500);
+  if(700<value_ldr){ 
+      if( value_pir==HIGH){
+          digitalWrite(LED,HIGH);  // Turn ON the light
+          if(pirState == LOW){
+              value_pir==LOW;
+          }  
+      } 
+      else {
+          digitalWrite(LED,LOW); // Turn OFF the light
+          if(pirState == HIGH){
+              pirState = LOW;       
+          }  
+      } 
   }
- }  
- else {
-  
-    digitalWrite(LED,LOW); // Turn OFF the light
-       
- }
-  
+  else{
+    digitalWrite(LED,LOW);
+  }
 }
-
-
